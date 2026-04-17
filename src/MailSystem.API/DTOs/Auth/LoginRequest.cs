b2542abc3f9e9559c6e3ae06bee86db75939ede7
hussam <hussamishtiaq0@gmail.com>
2026-04-17 19:13:39 +0500

    Created Auth DTOs and Auth Controller

---
+++ src/MailSystem.API/DTOs/Auth/LoginRequest.cs
@@ -0,0 +1,7 @@
+namespace MailSystem.API.DTOs.Auth;
+
+public class LoginRequest
+{
+    public string Email { get; set; } = default!;
+    public string Password { get; set; } = default!;
+}
---
+++ src/MailSystem.API/DTOs/Auth/RegisterRequest.cs
@@ -0,0 +1,8 @@
+namespace MailSystem.API.DTOs.Auth;
+
+public class RegisterRequest
+{
+    public string FullName { get; set; } = default!;
+    public string Email { get; set; } = default!;
+    public string Password { get; set; } = default!;
+}
